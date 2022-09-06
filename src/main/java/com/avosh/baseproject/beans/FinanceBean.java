0828f3981757185a6ee344deedb7de6a6acb189d
Ryan <amirkhany@gmail.com>
2022-09-06 19:55:59 +0330

    FinanceBean is changed - Ryan

--- src/main/java/com/avosh/baseproject/beans/FinanceBean.java
+++ src/main/java/com/avosh/baseproject/beans/FinanceBean.java
@@ -11,9 +11,12 @@ package com.avosh.baseproject.beans;
 import com.avosh.baseproject.dto.FinanceDto;
 import com.avosh.baseproject.dto.NewsDto;
 import com.avosh.baseproject.services.FinanceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -23,14 +26,28 @@ public class FinanceBean extends BaseBean<FinanceService, FinanceDto> {
     private List<FinanceDto> dtoList;
     private boolean isEditMode;
 
+    @Autowired
+    public void setService(FinanceService service) {
+        this.service = service;
+    }
+
+    @PostConstruct
+    public void init() {
+        isEditMode = false;
+        setDto(new FinanceDto());
+        financeDto = new FinanceDto();
+        dtoList = new ArrayList<>();
+        dtoList = service.retrieveAll();
+
+    }
+
     @Override
     public void save() {
 
     }
 
 
-
-      public void deleteRecord() {
+    public void deleteRecord() {
 
     }
 
@@ -40,6 +57,7 @@ public class FinanceBean extends BaseBean<FinanceService, FinanceDto> {
 
     public void setFinanceDto(FinanceDto financeDto) {
         this.financeDto = financeDto;
+        setDto(financeDto);
     }
 
     public List<FinanceDto> getDtoList() {
