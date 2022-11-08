b6515038b6deb6d237b9ff4b7f1cec286b5e55c1
Amirkhani <amirkhany@gmail.com>
2022-11-08 16:44:59 +0330

    ResourceBundleMessageSource is created and ready to use - Ryan

--- src/main/java/com/avosh/baseproject/beans/BaseBean.java
+++ src/main/java/com/avosh/baseproject/beans/BaseBean.java
@@ -8,14 +8,19 @@ import com.avosh.baseproject.dto.BaseDto;
 import com.avosh.baseproject.excptions.BaseException;
 import com.avosh.baseproject.services.BaseService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Locale;
 
 
 public abstract class BaseBean<SRV extends BaseService, D extends BaseDto>  implements Serializable {
+    @Autowired
+    private MessageSource messageSource;
     private static final Logger log = Logger.getLogger(BaseBean.class);
     private boolean isEditMode;
     protected SRV service;
@@ -57,6 +62,7 @@ public abstract class BaseBean<SRV extends BaseService, D extends BaseDto>  impl
     public  void save(){
         try {
             service.save(getDto());
+            messageSource.getMessage("app.header", null, Locale.ENGLISH);
             showMessage("Done!");
         } catch (Exception e) {
             log.error(e);
---
+++ src/main/java/com/avosh/baseproject/conf/AppConfig.java
@@ -0,0 +1,19 @@
+package com.avosh.baseproject.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("locale/message");
+        source.setUseCodeAsDefaultMessage(true);
+
+        return source;
+    }
+}
