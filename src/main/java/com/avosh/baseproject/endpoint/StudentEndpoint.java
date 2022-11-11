5e7a4276a0ca858f5736486012a9ad445b954eda
Ryan <amirkhany@gmail.com>
2022-11-11 14:58:02 +0330

    Soap service is configured - Ryan

--- src/main/java/com/avosh/baseproject/endpoint/StudentEndpoint.java
+++ src/main/java/com/avosh/baseproject/endpoint/StudentEndpoint.java
@@ -4,7 +4,7 @@ package com.avosh.baseproject.endpoint;
 import com.avosh.baseproject.model.Student;
 import com.avosh.baseproject.model.request.StudentDetailsRequest;
 import com.avosh.baseproject.model.response.StudentDetailsResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.apache.log4j.Logger;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -12,15 +12,18 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class StudentEndpoint extends AbstractEndpoint{
+    private static final Logger log = Logger.getLogger(StudentEndpoint.class);
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "StudentDetailsRequest")
     @ResponsePayload
     public StudentDetailsResponse getStudent(@RequestPayload StudentDetailsRequest request) {
+        log.info("StudentEndpoint is Started");
         StudentDetailsResponse response = new StudentDetailsResponse();
         Student res =  new Student();
         res.setAddress("address");
-        res.setName("name");
+        res.setName(request.getName());
         res.setStandard(1);
         response.setStudent(res);
+        log.info("StudentEndpoint is Ended");
         return response;
     }
 }
--- src/main/java/com/avosh/baseproject/model/request/package-info.java
+++ src/main/java/com/avosh/baseproject/model/request/package-info.java
@@ -1,2 +1,2 @@
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.avosh.com/xml/definitions", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.avosh.baseproject.conf;
+package com.avosh.baseproject.model.request;
