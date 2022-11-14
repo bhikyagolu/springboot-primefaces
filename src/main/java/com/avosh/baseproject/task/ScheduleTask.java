a60345abd5c77e58f3332db45c2c7001fd6ef4ca
Amirkhani <amirkhany@gmail.com>
2022-11-14 15:27:33 +0330

    some improvements - Ryan

--- src/main/java/com/avosh/baseproject/task/ScheduleTask.java
+++ src/main/java/com/avosh/baseproject/task/ScheduleTask.java
@@ -22,6 +22,6 @@ public class ScheduleTask {
     @Scheduled(fixedRate = 100000)
     @CheckScheduler(schedulerId = 1)
     public void run() {
-        System.out.println("task is run");
+        System.out.println("task is running");
     }
 }
