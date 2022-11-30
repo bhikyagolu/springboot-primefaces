2f979e4282ecb0da0f52fb4ce455315d73389c15
Amirkhani <amirkhany@gmail.com>
2022-11-30 15:42:13 +0330

    namespace for soap is changed - Ryan

--- src/main/java/com/avosh/baseproject/endpoint/AbstractEndpoint.java
+++ src/main/java/com/avosh/baseproject/endpoint/AbstractEndpoint.java
@@ -6,5 +6,5 @@ package com.avosh.baseproject.endpoint;
 
 public abstract class AbstractEndpoint {
     protected static final String NAMESPACE_URI = "http://www.avosh.com/xml/definitions";
-    protected static final String TEST_NAMESPACE_URI = "http://sabapardazesh.com/schema/testResponse";
+    protected static final String TEST_NAMESPACE_URI = "http://www.avosh.com/schema/testResponse";
 }
