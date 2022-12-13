8f034e65f246b6e718455be029c94ccaafa528b8
Amirkhani <amirkhany@gmail.com>
2022-12-13 10:22:54 +0330

    Transaction status enum is added - Ryan

---
+++ src/main/java/com/avosh/baseproject/enums/TransactionStatusEnum.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Created by Alireza Amirkhani 2022
+ ******************************************************************************/
+
+package com.avosh.baseproject.enums;
+
+public enum TransactionStatusEnum {
+    POST(100, "Done"),
+    ADS(101, "Failed");
+
+    private Integer status;
+    private String description;
+
+    TransactionStatusEnum(Integer type, String description) {
+        this.status = type;
+        this.description = description;
+    }
+
+    public static String valueByCode(Integer code) {
+        for (TransactionStatusEnum typeEnum : TransactionStatusEnum.values()) {
+            if (typeEnum.getStatus() == code) {
+                return typeEnum.description;
+            }
+        }
+        return null;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
