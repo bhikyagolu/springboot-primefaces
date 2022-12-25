c1117b1ab2b2ae437288073475a4183dde9c6a95
Amirkhani <amirkhany@gmail.com>
2022-12-25 08:24:06 +0330

    news model is updated - Ryan

--- src/main/java/com/avosh/baseproject/ws/model/NewsDetailResponse.java
+++ src/main/java/com/avosh/baseproject/ws/model/NewsDetailResponse.java
@@ -2,5 +2,21 @@ package com.avosh.baseproject.ws.model;
 
 public class NewsDetailResponse extends Response{
    private Page page;
+   private String detail;
 
+   public Page getPage() {
+      return page;
+   }
+
+   public void setPage(Page page) {
+      this.page = page;
+   }
+
+   public String getDetail() {
+      return detail;
+   }
+
+   public void setDetail(String detail) {
+      this.detail = detail;
+   }
 }
--- src/main/java/com/avosh/baseproject/ws/model/NewsResponse.java
+++ src/main/java/com/avosh/baseproject/ws/model/NewsResponse.java
@@ -1,6 +1,51 @@
 package com.avosh.baseproject.ws.model;
 
+import java.util.Date;
+
 public class NewsResponse  extends Response{
    private Page page;
+   private String title;
+   private String brif;
+   private String detail;
+   private Date dateTime;
 
+   public Page getPage() {
+      return page;
+   }
+
+   public void setPage(Page page) {
+      this.page = page;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getBrif() {
+      return brif;
+   }
+
+   public void setBrif(String brif) {
+      this.brif = brif;
+   }
+
+   public String getDetail() {
+      return detail;
+   }
+
+   public void setDetail(String detail) {
+      this.detail = detail;
+   }
+
+   public Date getDateTime() {
+      return dateTime;
+   }
+
+   public void setDateTime(Date dateTime) {
+      this.dateTime = dateTime;
+   }
 }
