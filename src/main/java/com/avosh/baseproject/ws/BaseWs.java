68ab2db367c12a92e01102fba808d2f7897a7b7f
Amirkhani <amirkhany@gmail.com>
2022-12-26 14:36:21 +0330

    isTokenValidate is added to base ws  - Ryan

--- src/main/java/com/avosh/baseproject/ws/BaseWs.java
+++ src/main/java/com/avosh/baseproject/ws/BaseWs.java
@@ -1,9 +1,23 @@
 package com.avosh.baseproject.ws;
 
+import com.avosh.baseproject.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public abstract class BaseWs {
-    //todo
-    public Boolean isTokenValid(){
-        return false;
+    @Autowired
+    private TokenService tokenService;
+
+    public Boolean isTokenValid(String token, String mac) {
+        try {
+            Boolean res = tokenService.isTokenValid(token, mac);
+            if (res) {
+                return true;
+            }else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
