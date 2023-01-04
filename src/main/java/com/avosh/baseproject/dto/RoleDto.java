4edba82a017ca9684af290f90112822a855aee6b
Amirkhani <amirkhany@gmail.com>
2023-01-04 12:45:35 +0330

    RoleDto is added - Ryan

---
+++ src/main/java/com/avosh/baseproject/dto/RoleDto.java
@@ -0,0 +1,29 @@
+package com.avosh.baseproject.dto;
+
+import java.io.Serializable;
+
+public class RoleDto implements Serializable {
+    private String role;
+    private String title;
+
+    public RoleDto(String role, String title) {
+        this.role = role;
+        this.title = title;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
