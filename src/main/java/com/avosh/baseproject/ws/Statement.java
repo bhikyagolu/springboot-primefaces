9043f245084c83d815a34f355b3b83cebf4360e7
Amirkhani <amirkhany@gmail.com>
2023-01-07 08:45:07 +0330

    statement webservice is added - Ryan

--- src/main/java/com/avosh/baseproject/ws/Statement.java
+++ src/main/java/com/avosh/baseproject/ws/Statement.java
@@ -7,15 +7,18 @@ import com.avosh.baseproject.ws.model.TransactionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ws")
-public class Balance extends BaseWs{
+public class Statement extends BaseWs{
     @Autowired
     private TransactionService transactionService;
 
-    @PostMapping("/account/blance")
+    @PostMapping("/account/statement")
     public ResponseEntity getBalance(@RequestHeader("authorization") String token){
         HttpStatus httpStatus = HttpStatus.OK;
         TransactionResponse response = new TransactionResponse();
