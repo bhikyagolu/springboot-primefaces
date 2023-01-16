5efe8d3b1198d2bbf38b5e334586d0c64694c4a2
Ryan <amirkhany@gmail.com>
2023-01-16 09:31:01 +0330

    some improvements -- Ryan

--- src/main/java/com/avosh/baseproject/task/ScheduleTask.java
+++ src/main/java/com/avosh/baseproject/task/ScheduleTask.java
@@ -5,7 +5,9 @@
 package com.avosh.baseproject.task;
 
 import com.avosh.baseproject.validator.CheckScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
