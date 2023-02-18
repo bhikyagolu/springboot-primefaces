0188023d24998716c899e4f07d660741c73234b8
ryan <amirkhany@gmail.com>
2023-02-18 12:33:59 +0330

    BackendCommunicationException constructor is updated Ryan

--- src/main/java/com/avosh/baseproject/excptions/BackendCommunicationException.java
+++ src/main/java/com/avosh/baseproject/excptions/BackendCommunicationException.java
@@ -1,5 +1,14 @@
 package com.avosh.baseproject.excptions;
 
 public class BackendCommunicationException extends BaseException {
+    public BackendCommunicationException(String message) {
+        super(message);
+    }
 
+    public BackendCommunicationException(Throwable cause) {
+        super(cause);
+    }
+
+    public BackendCommunicationException() {
+    }
 }
